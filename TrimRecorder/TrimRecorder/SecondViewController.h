47a178fadd4ad3a6c7d6804d644f2d1a23e90a7b
perlman@163.com <douglas.zhang>
2015-03-07 11:05:38 +0800

    Create new project

---
+++ TrimRecorder/TrimRecorder/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  TrimRecorder
+//
+//  Created by JianminZhang on 15/3/7.
+//  Copyright (c) 2015年 Douglas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
