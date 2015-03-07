93e8d12c36e4bbaf42982509ef1567b922b386c0
perlman@163.com <douglas.zhang>
2015-03-07 11:00:17 +0800

    Initial Commit

---
+++ TrimRecorder/TrimProject/TrimProject/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  TrimProject
+//
+//  Created by JianminZhang on 15/3/7.
+//  Copyright (c) 2015年 Douglas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
